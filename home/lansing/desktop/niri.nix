ec428aa3928bd4375660f22687785806cd2cf370
simlans <55317770+simlans@users.noreply.github.com>
2026-05-02 19:07:04 +0200

    Pick niri wallpaper from ~/Pictures/Wallpapers, fall back to nixos-artwork

--- home/lansing/desktop/niri.nix
+++ home/lansing/desktop/niri.nix
@@ -1,4 +1,14 @@
-{ ... }:
+{ pkgs, ... }:
+let
+  wallpaper = "${pkgs.nixos-artwork.wallpapers.simple-blue}/share/backgrounds/nixos/nix-wallpaper-simple-blue.png";
+in
 {
-  xdg.configFile."niri/config.kdl".source = ./niri.kdl;
+  xdg.configFile."niri/config.kdl".text = builtins.replaceStrings
+    [ "@WALLPAPER@" ]
+    [ wallpaper ]
+    (builtins.readFile ./niri.kdl);
+
+  home.packages = with pkgs; [
+    swaybg
+  ];
 }
